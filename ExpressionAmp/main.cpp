f334c1649f8f39931a57db63f91d0b3e889ab4c7
alex-B09 <alex.boulanger@usherbrooke.ca>
2014-02-11 23:27:36 -0500

    Change from c++03 typedef to c++11 using types. Remove unwanted stuff Create traits for obtaining information on types - values

--- ExpressionAmp/main.cpp
+++ ExpressionAmp/main.cpp
@@ -4,20 +4,37 @@
 #include <vector>
 #include <utility>
 
+
+//----------------------------------------------
+//traits declaration
+
+template <class M>
+struct DataTypeTraits;
+
+template <class M>
+struct RankTraits;
+
+template <class M>
+struct ArrayTypeTraits;
+
+//----------------------------------------------
+
+
+
 // TODO will not work pass 2D -- limitation for now...
 // template maybe?
-template <int D, typename T>
+template <int R, typename T>
 class Matrix
 {
 public:
-    enum {DIMENSION = D};
+    enum {RANK = R};
 
-    typedef T dataType;
+    using dataType = T;
 
     // the choice of using a concurrency::array is to force the data in the 
-    typedef concurrency::array<dataType, D> arrayType;
-    typedef concurrency::extent<D> shapeType;
-    typedef std::vector<dataType> dataContainer;
+    using arrayType = concurrency::array<dataType, RANK>;
+    using shapeType = concurrency::extent<RANK>;
+    using dataContainer = std::vector<dataType>;
 
 private:
     shapeType m_shape;
@@ -36,139 +53,210 @@ public:
         m_data = arrayType(m_shape, data.begin(), data.end());
     }
 
-    std::vector<dataType> GetData()
+    arrayType GetData()
     {
         return m_data;
     }
 };
 
-
-// This is not much usefull for now but can be usefull with more matrix types in the future
-template <class M>
-struct MatrixTraits;
-
-template<int D, typename T>
-struct MatrixTraits<Matrix<D, T>>
-{
-    enum { Dimension = D};
-    typedef T type;
-    typedef typename Matrix<D,T>::shapeType shapeType;
-
-    //typedef typename Matrix<D, T>::dataType dataType;
-};
-// other traits if needed
-
-typedef Matrix<2, int> Matrix2f;
-
-template <class T> // should be a matrix or a vector...either way, we should extract information on it
-class Variable
-{
-public:
-    enum { Dimension = MatrixTraits<T>::Dimension };
-    typedef typename MatrixTraits<T>::type dataType;
-    typedef typename MatrixTraits<T>::shapeType shapeType;
-   
-private:
-    T & m_variable;
-
-
-public:
-    Variable(T & variable)
-        :m_variable(variable)
-    {
-    }
-
-    int GetValues() const
-    {
-        // TODO
-        return 1;
-    }
-};
-
-//template <typename T>
-//struct Constant
-//{
-//    T m_value;
-//
-//    Constant(T && value)
-//        :m_value(std::move(value))
-//    {
-//    }
-//};
-
+template <class Element>
 struct Expression
 {
-    // TODO
+    Element m_element;
+
+    Expression(Element & element)
+        :m_element(element)
+    {
+    }
+
+    // operator () avec return Type Traits
+    auto operator()() -> ArrayTypeTraits<Element>
+    {
+        return m_element.GetValues();
+    }
 };
 
 
-template <class element1, class element2, class opp>
+template <class Element1, class Element2, class Opp>
 struct ComplexExpression
 {
-    // TODO check if copy is ok
-    element1 & m_leftOperand;
-    element2 & m_rightOperand;
+    using expressionLeft = Expression<Element1>;
+    using expressionRight = Expression<Element2>;
 
-    ComplexExpression(element1 & left, element2 & right)
+    // TODO check if copy is ok
+    expressionLeft & m_leftOperand;
+    expressionRight & m_rightOperand;
+
+    ComplexExpression(expressionLeft & left, expressionRight & right)
         : m_leftOperand(left)
         , m_rightOperand(right)
     {
     }
 
-    
-    auto operator()() -> decltype(opp::apply(element1, element2))
+    // do something about no reference.... actualy, the return move semantics should do fine...
+    auto operator()()-> decltype(Opp::apply(expressionLeft, expressionRight))
     {
-        return opp::apply(m_element, m_element2)
+        return Opp::apply(m_leftOperand, m_rightOperand)
     }
-
-
 };
 
+//----------------------------------------------
+// traits
+template <class Element1, class Element2, class Opp>
+struct DataTypeTraitsOppBased;
+
+template <int Rank1, int Rank2, class Opp>
+struct RankTraitsOppBased;
+
+// general matrix template
+template <class M>
+struct DataTypeTraits
+{
+    using dataType = typename M::dataType;
+};
+
+// TODO at some point, constant...
+template <class M>
+struct DataTypeTraits<Expression<M>>
+{
+    using dataType = typename DataTypeTraits<M>::dataType;
+};
+
+// Complex Expression
+template <class Element1, class Element2, class Opp>
+struct DataTypeTraits<ComplexExpression<Element1, Element2, Opp>>
+{
+    using ComplexExpr = ComplexExpression<Element1, Element2, Opp>;
+
+    using LeftDataType = typename DataTypeTraits<typename ComplexExpr::ExpressionLeft>::dataType;
+    using RightDataType = typename DataTypeTraits<typename ComplexExpr::ExpressionRight>::dataType;
+
+    using dataType = typename DataTypeTraitsOppBased<LeftDataType, RightDataType, Opp>::dataType;
+};
+
+
+template <class M>
+struct RankTraits
+{
+    enum{ RANK = M::RANK};
+};
+
+template <class M>
+struct RankTraits<Expression<M>>
+{
+    enum{ RANK = RankTraits<M>::RANK };
+};
+
+template <class Element1, class Element2, class Opp>
+struct RankTraits<ComplexExpression<Element1, Element2, Opp>>
+{
+    using ComplexExpr = ComplexExpression<Element1, Element2, Opp>;
+
+    enum
+    {
+        LeftDataRank = typename RankTraits<typename ComplexExpr::ExpressionLeft>::RANK,
+        RightDataRank = typename RankTraits<typename ComplexExpr::ExpressionRight>::RANK,
+
+        RANK = RankTraitsOppBased<LeftDataRank, RightDataRank, Add>::RANK;
+    };
+};
+
+// general matrix template
+template <class M>
+struct ArrayTypeTraits
+{
+    using arrayType = typename M::arrayType;
+};
+
+// TODO at some point, constant...
+template <class M>
+struct ArrayTypeTraits<Expression<M>>
+{
+    using arrayType = typename ArrayTypeTraits<M>::arrayType;
+};
+
+// Complex Expression
+template <class Element1, class Element2, class Opp>
+struct ArrayTypeTraits<ComplexExpression<Element1, Element2, Opp>>
+{
+    using ComplexExpr = ComplexExpression<Element1, Element2, Opp>;
+    using dataType = typename DataTypeTraits<ComplexExpr>::dataType;
+
+    enum { RANK = RankTraits<ComplexExpr>::RANK };
+
+    using arrayType = concurrency::array<dataType, RANK>;
+};
+
+// end traits
+//----------------------------------------------
+
+
+
+//------------------------------------
+// Opps
 struct Add
 {
     // I still have to find a good return value....a variable maybe??
-    template<class element1, class element2>
-    static auto apply(Variable<element1> left, Variable<element1> right) 
-        ->concurrency::array<decltype(std::declval<Variable<element1>::dataType>() + std::declval<Variable<element2>::dataType>()), Variable<element2>::DIMENSION>
+    template<class LeftExpression, class RightExpression>
+    static auto apply(LeftExpression left, RightExpression right)
+        ->typename ArrayTypeTraits<ComplexExpression<LeftExpression, RightExpression, Add>>::arrayType
     {
-        typedef decltype(std::declval<Variable<element1>::dataType>() + std::declval<Variable<element2>::dataType>()), Variable<element2>::DIMENSION> dataType;
-        const int DIMENSION = Variable<element2>::DIMENSION;
-
+        using ComplexExpr = ComplexExpression<LeftExpression, RightExpression, Add>;
+        using arrayType = typename ArrayTypeTraits<ComplexExpr>::arrayType;
         
+        const int RANK = RankTraits<ComplexExpr>::RANK;
+
         //-------------------------------------------------------
         // TODO something for the extent
-        concurrency::array<dataType, DIMENSION> returnValue(element2.extent);
-        auto & leftValues = left.GetValues();
-        auto & rightValues = right.GetValues();
+        arrayType returnValue(concurrency::extent<RANK>(5, 2));
+        auto & leftValues = left();
+        auto & rightValues = right();
 
         concurrency::parallel_for_each(
             returnValue.extent,
-            [=](concurrency::index<DIMENSION> idx) restrict(amp)
+            [=](concurrency::index<returnValue.extent> idx) restrict(amp)
             {
                 returnValue[idx] = leftValues[idx] + rightValues[idx];
             });
 
-
-        return concurrency::array<dataType, DIMENSION>();
-
+        return returnValue;
     }
+};
+//------------------------------------
 
-    // TODO maybe static assert on rank difference...
+
+//------------------------------------
+// opperation based Traits
+template <class DataType1, class DataType2>
+struct DataTypeTraitsOppBased<DataType1, DataType2, Add>
+{
+    using dataType = decltype(std::declval<Element1>() + std::declval<DataType2>());
 };
 
-template <class C>
-struct OpperationTraits;
-// TODO
+template <int Rank1, int Rank2>
+struct RankTraitsOppBased<Rank1, Rank2, Add>
+{
+    enum { RANK = Rank1};
+};
+//------------------------------------
 
 
+
+// TODO operator +
+// TODO constructor for Comprex expression that can take an expression, complex expression
+//      and the mixture of the 2
+
+
+
+typedef Matrix<2, int> Matrix2f;
 
 int main()
 {
     using std::vector;
-    Matrix2f::shapeType shape(2, 5);
+    Matrix2f::shapeType shape(5, 2);
 
-    //vector<int> ve1 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    //vector<int> ve2 = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+    vector<int> ve1 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    vector<int> ve2 = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
 
     Matrix2f m1(shape);
     Matrix2f m2(shape);
